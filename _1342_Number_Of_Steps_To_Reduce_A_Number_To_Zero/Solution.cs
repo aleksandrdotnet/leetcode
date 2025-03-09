c388b34c707415efb2c33debec9ff39f8fff38fd
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-03-09 21:56:10 +0100

    1342 Number of Steps to Reduce a Number to Zero

---
+++ _1342_Number_Of_Steps_To_Reduce_A_Number_To_Zero/Solution.cs
@@ -0,0 +1,20 @@
+namespace _1342_Number_Of_Steps_To_Reduce_A_Number_To_Zero;
+
+public class Solution
+{
+    public static int Run(int input)
+    {
+        var result = 0;
+        while(input != 0)
+        {
+            if(input % 2 == 0)
+                input /= 2;
+            else
+                input--;
+
+            result++;
+        }
+
+        return result;
+    }
+}
---
+++ _1342_Number_Of_Steps_To_Reduce_A_Number_To_Zero/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace _1342_Number_Of_Steps_To_Reduce_A_Number_To_Zero;
+
+public class Test
+{
+    [Theory]
+    [InlineData(14, 6)]
+    [InlineData(8, 4)]
+    [InlineData(123, 12)]
+    public void Run(int input, int output)
+    {
+        var result = Solution.Run(input);
+
+        Assert.Equal(output, result);
+    }
+}
