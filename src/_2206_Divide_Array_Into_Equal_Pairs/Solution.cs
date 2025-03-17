fd7739d1d8c002e9f9a6cd1a7f046761f6bceac6
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-03-17 15:45:53 +0100

    2206 Divide Array Into Equal Pairs

---
+++ src/_2206_Divide_Array_Into_Equal_Pairs/Solution.cs
@@ -0,0 +1,19 @@
+namespace _2206_Divide_Array_Into_Equal_Pairs;
+
+public class Solution
+{
+    public bool DivideArray(int[] nums)
+    {
+        if (nums.Length % 2 != 0)
+            return false;
+
+        var dict = new Dictionary<int, int>();
+        foreach (var n in nums)
+        {
+            if (!dict.TryAdd(n, 1))
+                dict[n]++;
+        }
+
+        return dict.All(x => x.Value % 2 == 0);
+    }
+}
---
+++ src/_2206_Divide_Array_Into_Equal_Pairs/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace _2206_Divide_Array_Into_Equal_Pairs;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 3, 2, 3, 2, 2, 2 }, true)]
+    [InlineData(new[] { 1, 2, 3, 4 }, false)]
+    public void CommonFactors(int[] a, bool expected)
+    {
+        var result = new Solution().DivideArray(a);
+        Assert.Equal(expected, result);
+    }
+}
