173b73a7b97068ae2f5b0b0a991085947564edfd
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-03-18 10:34:52 +0100

    26 Remove Duplicates from Sorted Array

---
+++ src/_26_Remove_Duplicates_from_Sorted_Array/Solution.cs
@@ -0,0 +1,17 @@
+namespace _26_Remove_Duplicates_from_Sorted_Array;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+
+        if (nums.Length < 2) return nums.Length;
+
+        var j = 0;
+        for (var i = 0; i < nums.Length; i++)
+            if (nums[j] != nums[i])
+                nums[++j] = nums[i];
+
+        return j + 1;
+    }
+}
---
+++ src/_26_Remove_Duplicates_from_Sorted_Array/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace _26_Remove_Duplicates_from_Sorted_Array;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 1, 1, 2 }, 2)]
+    [InlineData(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+    public void Run(int[] input, int expected)
+    {
+        var result = new Solution().RemoveDuplicates(input);
+        Assert.Equal(expected, result);
+    }
+}
