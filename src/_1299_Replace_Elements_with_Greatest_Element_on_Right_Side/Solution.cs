4ac7366613da08768871a16295283947c44d029a
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-03-21 13:36:25 +0100

    1299 Replace Elements with Greatest Element on Right Side

---
+++ src/_1299_Replace_Elements_with_Greatest_Element_on_Right_Side/Solution.cs
@@ -0,0 +1,58 @@
+namespace _1299_Replace_Elements_with_Greatest_Element_on_Right_Side;
+
+public class Solution
+{
+    public int[] ReplaceElements2(int[] arr)
+    {
+        if (arr.Length == 1)
+            return [-1];
+        if (arr.Length == 2) return [arr[1], -1];
+
+        var result = new int[arr.Length];
+        result[arr.Length - 1] = -1;
+
+        var maxIndex = 0;
+        while (maxIndex < arr.Length - 1)
+        {
+            var init = maxIndex;
+            var max = -1;
+            for (var j = maxIndex + 1; j < arr.Length; j++)
+            {
+                if (max < arr[j])
+                {
+                    max = arr[j];
+                    maxIndex = j;
+                }
+            }
+
+            for (var j = init; j < maxIndex; j++)
+            {
+                result[j] = arr[maxIndex];
+            }
+        }
+
+        return result;
+    }
+
+    public int[] ReplaceElements(int[] arr)
+    {
+        if (arr.Length == 1)
+            return [-1];
+        if (arr.Length == 2) return [arr[1], -1];
+
+        var last = -1;
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            if (arr[i] > last)
+            {
+                (arr[i], last) = (last, arr[i]);
+            }
+            else
+            {
+                arr[i] = last;
+            }
+        }
+
+        return arr;
+    }
+}
---
+++ src/_1299_Replace_Elements_with_Greatest_Element_on_Right_Side/Test.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace _1299_Replace_Elements_with_Greatest_Element_on_Right_Side;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 17, 18, 5, 4, 6, 1 }, new[] { 18, 6, 6, 6, 1, -1 })]
+    [InlineData(new[] { 400 }, new[] { -1 })]
+    public void Run(int[] input, int[] expected)
+    {
+        var result = new Solution().ReplaceElements(input);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+}
