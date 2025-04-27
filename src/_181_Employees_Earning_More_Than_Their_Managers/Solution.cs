5d0c1839a1addaf5c37262169596f82af224472a
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-04-27 09:41:21 +0200

    181. Employees Earning More Than Their Managers

---
+++ src/_181_Employees_Earning_More_Than_Their_Managers/Solution.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace _181_Employees_Earning_More_Than_Their_Managers;
+
+public class Solution
+{
+    public class AppDbContext : DbContext
+    {
+        // ReSharper disable once MemberHidesStaticFromOuterClass
+        public DbSet<Employee> Employee { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseInMemoryDatabase("TestDb");
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(255)
+                    .IsRequired();
+
+                entity.HasData(
+                    new Employee { Id = 1, Name = "Joe", Salary = 70000, ManagerId = 3 },
+                    new Employee { Id = 2, Name = "Henry", Salary = 80000, ManagerId = 4 },
+                    new Employee { Id = 3, Name = "Sam", Salary = 60000, ManagerId = null },
+                    new Employee { Id = 4, Name = "Max", Salary = 90000, ManagerId = null }
+                );
+            });
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = null!;
+        public int Salary { get; init; }
+        public int? ManagerId { get; init; }
+    }
+
+}
---
+++ src/_181_Employees_Earning_More_Than_Their_Managers/Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _181_Employees_Earning_More_Than_Their_Managers;
+
+public class Test
+{
+    [Fact]
+    public void Query_Should_Return_Correct_Result()
+    {
+        using var context = new Solution.AppDbContext();
+
+        context.Database.OpenConnection();
+
+        context.Database.EnsureCreated();
+
+        var result = context.Database.SqlQuery<Result>($@"
+select 
+    emp.name [Employee]
+from Employee emp
+inner join Employee mngr on emp.managerId = mngr.id
+where emp.salary > mngr.salary")
+            .ToList();
+
+        var expected = new List<Result>
+        {
+            new() { Employee = "Joe" }
+        };
+
+        Assert.Equal(expected.Count, result.Count);
+        Assert.Equivalent(expected, result);
+    }
+
+
+    private class Result
+    {
+        public required string Employee { get; set; }
+    }
+}
