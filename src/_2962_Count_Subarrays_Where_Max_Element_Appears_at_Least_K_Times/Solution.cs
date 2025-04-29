3e716f57da384fe35bda3b847805da56b8a6a8c1
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-04-29 23:22:07 +0200

    2962. Count Subarrays Where Max Element Appears at Least K Times

---
+++ src/_2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times/Solution.cs
@@ -0,0 +1,27 @@
+namespace _2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times;
+
+public class Solution
+{
+    public long CountSubarrays(int[] nums, long k)
+    {
+        var result = 0L;
+        var count = 0L;
+        
+        var max = nums.Max();
+        for (int l = 0, r = 0; r < nums.Length; r++)
+        {
+            if(nums[r] == max)
+                count++;
+
+            while (count == k)
+            {
+                if (nums[l++] == max)
+                    count--;
+            }
+            
+            result += l;
+        }
+        
+        return result;
+    }
+}
---
+++ src/_2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace _2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 2, 3, 3 }, 2, 6)]
+    [InlineData(new[] { 1, 4, 2, 1 }, 3, 0)]
+    public void Run(int[] nums, long k, long expected)
+    {
+        var result = new Solution().CountSubarrays(nums, k);
+        Assert.Equal(expected, result);
+    }
+}
