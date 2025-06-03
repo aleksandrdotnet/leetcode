b23147e9c3523dc345196e9dbb061a751e3c2b11
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-06-03 19:35:49 +0200

    1661. Average Time of Process per Machine

---
+++ src/_1661_Average_Time_of_Process_per_Machine/Solution.cs
@@ -0,0 +1,66 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _1661_Average_Time_of_Process_per_Machine;
+
+public class Solution
+{
+
+    public class Activity
+    {
+        public int machine_id { get; set; }
+
+        public int process_id { get; set; }
+
+        public string activity_type { get; set; }
+
+        public double timestamp { get; set; }
+    }
+
+    public class ActivityConfiguration : IEntityTypeConfiguration<Activity>
+    {
+        public void Configure(EntityTypeBuilder<Activity> builder)
+        {
+            builder.ToTable("Activity");
+
+            builder.HasKey(a => new { MachineId = a.machine_id, ProcessId = a.process_id, ActivityType = a.activity_type, Timestamp = a.timestamp });
+
+            builder.Property(a => a.activity_type)
+                .HasConversion<string>() // enum → string
+                .IsRequired();
+
+            builder.Property(a => a.timestamp)
+                .IsRequired();
+        }
+    }
+    
+    public class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ActivityConfiguration());
+
+            modelBuilder.Entity<Activity>().HasData(
+                new Activity { machine_id = 0, process_id = 0, activity_type = "Start", timestamp = 0.712 },
+                new Activity { machine_id = 0, process_id = 0, activity_type = "End", timestamp = 1.52 },
+                new Activity { machine_id = 0, process_id = 1, activity_type = "Start", timestamp = 3.14 },
+                new Activity { machine_id = 0, process_id = 1, activity_type = "End", timestamp = 4.12 },
+                new Activity { machine_id = 1, process_id = 0, activity_type = "Start", timestamp = 0.55 },
+                new Activity { machine_id = 1, process_id = 0, activity_type = "End", timestamp = 1.55 },
+                new Activity { machine_id = 1, process_id = 1, activity_type = "Start", timestamp = 0.43 },
+                new Activity { machine_id = 1, process_id = 1, activity_type = "End", timestamp = 1.42 },
+                new Activity { machine_id = 2, process_id = 0, activity_type = "Start", timestamp = 4.1 },
+                new Activity { machine_id = 2, process_id = 0, activity_type = "End", timestamp = 4.512 },
+                new Activity { machine_id = 2, process_id = 1, activity_type = "Start", timestamp = 2.5 },
+                new Activity { machine_id = 2, process_id = 1, activity_type = "End", timestamp = 5.0 }
+            );
+        }
+    }
+}
---
+++ src/_1661_Average_Time_of_Process_per_Machine/Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _1661_Average_Time_of_Process_per_Machine;
+
+public class Test
+{
+    [Fact]
+    public void Query_Should_Return_Correct_Result()
+    {
+        using var context = new Solution.AppDbContext();
+
+        context.Database.OpenConnection();
+
+        context.Database.EnsureCreated();
+
+        var result = context.Database.SqlQuery<Result>(
+                $@"
+select
+    a1.machine_id,
+    round(avg(a2.timestamp) - avg(a1.timestamp), 3) [processing_time]
+from
+    Activity a1
+        inner join Activity a2 on a1.machine_id = a2.machine_id
+        and a1.process_id = a2.process_id
+        and a1.activity_type <> a2.activity_type
+where
+    a1.activity_type = 'Start'
+group by
+    a1.machine_id")
+            .ToList();
+
+
+        var expected = new List<(int machine_id, double processing_time)>
+        {
+            (0, 0.894),
+            (1, 0.995),
+            (2, 1.456)
+        };
+
+        Assert.Equal(expected.Count, result.Count);
+        foreach (var exp in expected) Assert.Contains(result, r => r.machine_id == exp.machine_id && r.processing_time == exp.processing_time);
+    }
+
+    public class Result
+    {
+        public int machine_id { get; set; }
+        public double processing_time { get; set; }
+    }
+}
