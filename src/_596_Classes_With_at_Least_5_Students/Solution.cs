836c7af1a55c4f66acd4098357370359faf413c5
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-06-08 19:31:09 +0200

    596. Classes With at Least 5 Students

---
+++ src/_596_Classes_With_at_Least_5_Students/Solution.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace _596_Classes_With_at_Least_5_Students;
+
+public class Solution
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseInMemoryDatabase("TestDb");
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.ToTable("Courses");
+                entity.HasKey(x => new { x.Student, x.Class });
+
+                entity.Property(e => e.Student).HasColumnName("student").HasMaxLength(255);
+                entity.Property(e => e.Class).HasColumnName("class").HasMaxLength(255);
+
+                entity.HasData(
+                    new Course { Student = "A", Class = "Math" },
+                    new Course { Student = "B", Class = "English" },
+                    new Course { Student = "C", Class = "Math" },
+                    new Course { Student = "D", Class = "Biology" },
+                    new Course { Student = "E", Class = "Math" },
+                    new Course { Student = "F", Class = "Computer" },
+                    new Course { Student = "G", Class = "Math" },
+                    new Course { Student = "H", Class = "Math" },
+                    new Course { Student = "I", Class = "Math" }
+                );
+            });
+        }
+    }
+    
+    public class Course
+    {
+        public string Student { get; set; } = null!;
+        public string Class { get; set; } = null!;
+    }
+}
---
+++ src/_596_Classes_With_at_Least_5_Students/Test.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace _596_Classes_With_at_Least_5_Students;
+
+public class Test
+{
+    [Fact]
+    public void Query_Should_Return_Correct_Result()
+    {
+        using var context = new Solution.AppDbContext();
+
+        context.Database.OpenConnection();
+
+        context.Database.EnsureCreated();
+
+        var result = context.Database.SqlQuery<Result>($@"
+select
+    c.class []
+from
+    Courses c
+group by
+    c.class
+having count(1) >= 5
+                             ")
+            .ToList();
+
+
+        var expected = new List<Result>
+        {
+            new("Math")
+        };
+
+        Assert.Equal(expected.Count, result.Count());
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    public class Result
+    {
+        public string Class { get; set; }
+
+        public Result()
+        {
+            
+        }
+
+        public Result(string @class)
+        {
+            Class = @class;
+        }
+    }
+}
