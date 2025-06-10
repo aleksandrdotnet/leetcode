df70f6226e937244c3595c74a34e0eb3b71a4a35
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-06-10 23:09:47 +0200

    619. Biggest Single Number

---
+++ src/_619_Biggest_Single_Number/Solution.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace _619_Biggest_Single_Number;
+
+public class Solution
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<MyNumber> MyNumbers { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseInMemoryDatabase("TestDb");
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyNumber>(entity =>
+            {
+                entity.ToTable("MyNumbers");
+                entity.HasKey(x => x.Id);
+
+                entity.Property(e => e.Num).HasColumnName("num");
+
+                entity.HasData(
+                    new MyNumber { Id = 1, Num = 8 },
+                    new MyNumber { Id = 2, Num = 8 },
+                    new MyNumber { Id = 3, Num = 3 },
+                    new MyNumber { Id = 4, Num = 3 },
+                    new MyNumber { Id = 5, Num = 1 },
+                    new MyNumber { Id = 6, Num = 4 },
+                    new MyNumber { Id = 7, Num = 5 },
+                    new MyNumber { Id = 8, Num = 6 }
+                );
+            });
+        }
+    }
+
+    public class MyNumber
+    {
+        public int Id { get; set; }
+        public int Num { get; set; }
+    }
+}
---
+++ src/_619_Biggest_Single_Number/Test.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace _619_Biggest_Single_Number;
+
+public class Test
+{
+    [Fact]
+    public void Query_Should_Return_Correct_Result()
+    {
+        using var context = new Solution.AppDbContext();
+
+        context.Database.OpenConnection();
+
+        context.Database.EnsureCreated();
+
+        var result = context.Database.SqlQuery<Result>($@"
+/* Write your T-SQL query statement below */
+;with cte as (select
+    mn.num
+from
+    MyNumbers mn
+group by 
+    mn.num
+having 
+    count(1) = 1
+)
+
+select
+    max(c.num) [num]
+from
+    cte c")
+            .ToList();
+
+
+        var expected = new List<Result>
+        {
+            new(6)
+        };
+
+        Assert.Equal(expected.Count, result.Count);
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    public class Result
+    {
+        public Result()
+        {
+        }
+
+        public Result(int num)
+        {
+            Num = num;
+        }
+        public int Num { get; set; }
+    }
+}
