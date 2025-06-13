b4e2050d62a6773f8d65daefc554e1cad995cc41
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-06-13 12:00:00 +0200

    610. Add new cases

--- src/_610_Triangle_Judgement/Solution.cs
+++ src/_610_Triangle_Judgement/Solution.cs
@@ -28,10 +28,7 @@ public class Solution
 
                 entity.HasData(
                     new Triangle { Id = 1, X = 13, Y = 15, Z = 30 },
-                    new Triangle { Id = 2, X = 10, Y = 20, Z = 15 },
-                    new Triangle { Id = 3, X = 90, Y = 20, Z = 15 },
-                    new Triangle { Id = 4, X = 40, Y = 20, Z = 15 },
-                    new Triangle { Id = 5, X = 140, Y = 20, Z = 15 }
+                    new Triangle { Id = 2, X = 10, Y = 20, Z = 15 }
                 );
             });
         }
