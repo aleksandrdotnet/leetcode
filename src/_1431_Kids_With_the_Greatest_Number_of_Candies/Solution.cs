9948a55be3d573fb7a021662caaf40735b8565aa
aleksandrdotnet <aleksandrdotnet@gmail.com>
2025-07-10 22:54:19 +0200

    1431. Kids With the Greatest Number of Candies

---
+++ src/_1431_Kids_With_the_Greatest_Number_of_Candies/Solution.cs
@@ -0,0 +1,21 @@
+namespace _1431_Kids_With_the_Greatest_Number_of_Candies;
+
+public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var max = candies[0];
+
+        var result = new bool[candies.Length];
+        foreach(var c in candies)
+            max = Math.Max(max, c);
+
+        for(var i = 0; i < candies.Length; i++)
+        {
+            if(candies[i] + extraCandies >= max)
+                result[i] = true;
+        }
+
+        return result;
+    }
+}
---
+++ src/_1431_Kids_With_the_Greatest_Number_of_Candies/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace _1431_Kids_With_the_Greatest_Number_of_Candies;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 2, 3, 5, 1, 3 }, 3, new[] { true, true, true, false, true })]
+    [InlineData(new[] { 4, 2, 1, 1, 2 }, 1, new[] { true, false, false, false, false })]
+    [InlineData(new[] { 12, 1, 12 }, 10, new[] { true, false, true })]
+    public void Run(int[] candies, int extraCandies, IList<bool> expected)
+    {
+        var result = new Solution().KidsWithCandies(candies, extraCandies);
+        Assert.Equal(expected, result);
+    }
+}
